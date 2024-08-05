e3cbca77ca824ac4208dbef468d9a77293fcabde
MAHESHA T <123284678+MAHESH-T07@users.noreply.github.com>
2024-08-05 12:04:16 +0530

    Add files via upload

--- stock_price_prediction.py
+++ stock_price_prediction.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import pandas as pd
 from pickle import load
+import datetime
+import numpy as np
 
 def create_page():
 	st.title('STOCK PRICE PREDICTION')
-	#yr_val = st.date_input('enter date in day-month-year format')
+	yr_val = st.date_input('enter date in day-month-year format')
 	st.write(yr_val)
-	
+	timestamp = datetime.datetime.combine(yr_val,datetime.datetime.min.time()).timestamp()
 
 	data = {
-		'year' :yr_val
+		'year' :[timestamp]
 		}
 
 	df = pd.DataFrame(data,index=[0])
@@ -17,6 +19,9 @@ def create_page():
 
 features = create_page()
 st.write(features)
+X_lstm = np.array([features['year'].values] * 10).reshape(1, 10, 1)
+
+
 
 loaded_model = load(open('lstm_model.pkl','rb'))
 res = loaded_model.predict(features)
